851f4e32298c976041236289028fc8e0a8733874
Joseph Muller <jtmuller5@gmail.com>
2022-11-13 10:33:17 -0500

    No data texts

--- lib/charts/calories_consumed_chart.dart
+++ lib/charts/calories_consumed_chart.dart
@@ -153,7 +153,9 @@ class CaloriesConsumedChart extends StatelessWidget {
                   ),
                 );
               } else{
-                return Container();
+                return Center(
+                  child: Text('No data'),
+                );
               }
             } else {
               return const Center(child: CircularProgressIndicator());
--- lib/charts/food_eaten_chart.dart
+++ lib/charts/food_eaten_chart.dart
@@ -38,7 +38,7 @@ class FoodEatenChart extends StatelessWidget {
       type: Chart.food,
       showExpand: showExpand,
       popup: popup,
-      title: 'Food Frwfrwg (g)/Treats Fed (g)',
+      title: 'Food Eaten (${metric ? 'g' : 'lbs'})/Treats Fed (${metric ? 'g' : 'lbs'})',
       //'Food Eaten (${model.metric ? 'g' : 'lbs'})/Treats Fed (${model.metric ? 'g' : 'lbs'})',
       chart: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
           stream: stream,
@@ -49,9 +49,12 @@ class FoodEatenChart extends StatelessWidget {
               List<Measurement> allMeasurements = snapshot.data?.docs.map((e) => Measurement.fromJson(e.data())).toList() ?? [];
               List<Measurement> rawMeasurements = allMeasurements
                   .where((measurement) =>
-                      (measurement.dateTime != null && measurement.dateTime!.isAfter(DateTime.now().subtract(selectedDateRange.duration))))
+                      (measurement.dateTime != null && measurement.dateTime!.isAfter(DateTime.now().subtract(selectedDateRange.duration)) &&
+                          measurement.offered != null))
                   .toList();
-              Map<int, BarChartGroupData> bars = {
+
+
+              Map<int, BarChartGroupData> bars = rawMeasurements.isNotEmpty ? {
                 for (var element in List.generate(
                   selectedDateRange.duration.inDays,
                   (index) => index,
@@ -64,79 +67,91 @@ class FoodEatenChart extends StatelessWidget {
                       )
                     ],
                   )
-              };
+              }: {};
 
+              debugPrint('RAW MEASUREMENT LENGTH: ${rawMeasurements.length}');
               for (Measurement measurement in (rawMeasurements)) {
-                if (measurement.offered != null && measurement.dateTime != null) {
+                if (measurement.offered != null && measurement.offered != 0 && (measurement.notEaten ?? 0) < measurement.offered! && measurement
+                    .dateTime !=
+                    null) {
                   double eaten = (measurement.offered! - (measurement.notEaten ?? 0)) / (metric ? 1 : 454);
                   double treats = (measurement.treats ?? 0) / (metric ? 1 : 454);
 
-                  if(eaten != 0) {
-                    bars[selectedDateRange.duration.inDays + measurement.dateTime!.difference(DateTime.now()).inDays] = BarChartGroupData(
-                      x: selectedDateRange.duration.inDays + measurement.dateTime!.difference(DateTime.now()).inDays,
-                      barRods: [
-                        BarChartRodData(
-                          y: eaten + treats,
-                          rodStackItems: [
-                            BarChartRodStackItem(0, eaten, color),
-                            if (measurement.treats != null) BarChartRodStackItem(eaten, eaten + treats, Colors.redAccent),
-                          ],
-                        )
-                      ],
-                    );
-                  }
+                  debugPrint('EATEN: $eaten');
+                  int day = selectedDateRange.duration.inDays + measurement.dateTime!.difference(DateTime.now()).inDays;
+                  debugPrint('DAY: $day');
+                  bars[day] = BarChartGroupData(
+                    x: day,
+                    barRods: [
+                      BarChartRodData(
+                        y: eaten + treats,
+                        rodStackItems: [
+                          BarChartRodStackItem(0, eaten, color),
+                          if (measurement.treats != null) BarChartRodStackItem(eaten, eaten + treats, Colors.redAccent),
+                        ],
+                      )
+                    ],
+                  );
                 }
               }
-              return Padding(
-                  padding: const EdgeInsets.only(right: 28.0),
-                  child: BarChart(
-                    BarChartData(
-                      gridData: FlGridData(drawVerticalLine: true),
-                      barGroups:  [],//bars.values.toList(),
-                      borderData: FlBorderData(
-                        border: const Border(
-                          left: BorderSide(
-                            width: 1,
-                            color: Colors.black54,
+
+              debugPrint('BARS LENGTH: ${bars.length}');
+              if (bars.values.isNotEmpty) {
+                return Padding(
+                    padding: const EdgeInsets.only(right: 28.0),
+                    child: BarChart(
+                      BarChartData(
+                        gridData: FlGridData(drawVerticalLine: true),
+                        barGroups: bars.values.toList(),
+                        borderData: FlBorderData(
+                          border: const Border(
+                            left: BorderSide(
+                              width: 1,
+                              color: Colors.black54,
+                            ),
+                            bottom: BorderSide(
+                              width: 1,
+                              color: Colors.black54,
+                            ),
                           ),
-                          bottom: BorderSide(
-                            width: 1,
-                            color: Colors.black54,
-                          ),
+                          show: true,
                         ),
-                        show: true,
+                        titlesData: FlTitlesData(
+                            rightTitles: SideTitles(showTitles: false),
+                            topTitles: SideTitles(showTitles: false),
+                            leftTitles: SideTitles(
+                              showTitles: true,
+                              reservedSize: 24,
+                              getTextStyles: (context, value) {
+                                return const TextStyle(fontSize: 10);
+                              },
+                              getTitles: (value) {
+                                return value.toStringAsFixed(metric ? 0 : 2);
+                              },
+                            ),
+                            bottomTitles: SideTitles(
+                              reservedSize: 0,
+                              showTitles: true,
+                              getTextStyles: (context, value) {
+                                return const TextStyle(fontSize: 10);
+                              },
+                              rotateAngle: 0,
+                              interval: selectedDateRange.verticalInterval,
+                              getTitles: (value) {
+                                return MaterialLocalizations.of(context).formatShortMonthDay(
+                                  DateTime.now().subtract(Duration(days: selectedDateRange.duration.inDays - value.toInt())),
+                                );
+                              },
+                            )),
                       ),
-                      titlesData: FlTitlesData(
-                          rightTitles: SideTitles(showTitles: false),
-                          topTitles: SideTitles(showTitles: false),
-                          leftTitles: SideTitles(
-                            showTitles: true,
-                            reservedSize: 24,
-                            getTextStyles: (context, value) {
-                              return const TextStyle(fontSize: 10);
-                            },
-                            getTitles: (value) {
-                              return value.toStringAsFixed(metric ? 0 : 2);
-                            },
-                          ),
-                          bottomTitles: SideTitles(
-                            reservedSize: 0,
-                            showTitles: true,
-                            getTextStyles: (context, value) {
-                              return const TextStyle(fontSize: 10);
-                            },
-                            rotateAngle: 0,
-                            interval: selectedDateRange.verticalInterval,
-                            getTitles: (value) {
-                              return MaterialLocalizations.of(context).formatShortMonthDay(
-                                DateTime.now().subtract(Duration(days: selectedDateRange.duration.inDays - value.toInt())),
-                              );
-                            },
-                          )),
-                    ),
-                    swapAnimationDuration: const Duration(milliseconds: 150), // Optional
-                    swapAnimationCurve: Curves.linear, // Optional
-                  ));
+                      swapAnimationDuration: const Duration(milliseconds: 150), // Optional
+                      swapAnimationCurve: Curves.linear, // Optional
+                    ));
+              } else {
+                return Center(
+                  child: Text('No data'),
+                );
+              }
             } else {
               return const Center(child: CircularProgressIndicator());
             }
